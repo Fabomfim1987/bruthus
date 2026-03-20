ee798c865290372f34dfaa1a9f69fd8b0edd6fe9
Fabomfim1987 <fabricio.bomfim@live.com>
2026-03-20 12:32:49 -0300

    Update datafast.js

--- api/datafast.js
+++ api/datafast.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+// v2export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
